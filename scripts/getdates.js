fe13fc592c1bf3cff6918c478de7aff1411578bc
moronialanis <moronialanis23@gmail.com>
2025-01-10 23:16:28 -0800

    change

---
+++ scripts/getdates.js
@@ -0,0 +1,14 @@
+const date = document.getFullYear;
+const fecha = document.getElementById('fecha');
+elemento.textContent =  <span class="date">${date}</span>;
+
+
+
+
+
+
+
+
+const ultimaModification = document.lastModified; 
+const elemento = document.getElementById('ultima-modification');
+elemento.textContent =  <span class="modification">${ultimaModification}</span>;
