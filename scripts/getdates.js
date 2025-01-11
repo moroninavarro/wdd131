a178a0e4b29097f224b7053ef224ec356040fdf6
moronialanis <moronialanis23@gmail.com>
2025-01-11 01:27:21 -0800

    change

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,14 +1,9 @@
 
+document.getElementById("CurrentYear").textContent = new Date().getFullYear();
 
 
-const lastModification = document.lastModified; 
-const elemento = document.getElementById('last-modification');
-elemento.textContent =  <span class="modification">${lastModification}</span>;
+document.getElementById("lastModified").textContent = document.lastModified;
 
 
 
 
-document.getElementById("currentyear").innerHTML = new Date().getFullYear();
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("currentyear").innerHTML = new Date().getFullYear();
-  });
