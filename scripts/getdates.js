9eede3219655282eb90a52fe1195ef0ec1b0b2ce
moronialanis <moronialanis23@gmail.com>
2025-01-11 01:05:12 -0800

    change

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,51 +1,14 @@
-// select the DOM elements for output
-const full = document.querySelector("#full");
-const worldfull = document.querySelector("#world-full");
-const short = document.querySelector("#short");
-const medium = document.querySelector("#medium");
-const year = document.querySelector("#year");
-const month = document.querySelector("#month");
-const day = document.querySelector("#day");
-const dayofweek = document.querySelector("#dayofweek");
 
-// use the date object
-const today = new Date();
 
-full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		dateStyle: "full"
-	}
-).format(today)}</span>`;
-worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat(
-	"en-UK",
-	{
-		dateStyle: "full"
-	}
-).format(today)}</span>`;
-short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		dateStyle: "short"
-	}
-).format(today)}</span>`;
-medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		dateStyle: "medium"
-	}
-).format(today)}</span>`;
 
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
-month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
-day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
-dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
+const lastModification = document.lastModified; 
+const elemento = document.getElementById('last-modification');
+elemento.textContent =  <span class="modification">${lastModification}</span>;
 
 
 
 
-
-
-const ultimaModification = document.lastModified; 
-const elemento = document.getElementById('ultima-modification');
-elemento.textContent =  <span class="modification">${ultimaModification}</span>;
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+  });
