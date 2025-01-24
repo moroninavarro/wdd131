f9797a1d8a7f39cb534b8713d9cfd228560e4f7c
moronialanis <moronialanis23@gmail.com>
2025-01-24 02:03:31 -0800

    change

--- scripts/place.js
+++ scripts/place.js
@@ -3,16 +3,25 @@ document.getElementById("CurrentYear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = document.lastModified;
 
+document.write(windChill.toFixed(2) +  " °F");
+
+
+
+let temperature = 10;
+let windSpeed = 5;
 
 function calculateWindChill (temperature, windSpeed) {
     return 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
 
 }
 
+if (temperature <=50 && windSpeed >3) {
+    let windChill = calculateWindChill(temperature, windSpeed);
+    document.write(windChill.toFixed(2) +  " °F");
+}
 
-document.addEventListener("DOMContentLoaded" , function(){
-    const temperature = 10;
-    const windSpeed= 5;
-    const windChill = calculateWindChill(temperature, windSpeed);
-    document.getElementById("windChillValue").innerHTML = windChill.toFixed(2) + " °F";
-    });
+else {
+    document.write("N/A");
+
+}
+
