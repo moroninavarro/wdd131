054952aa7bc8317184832cf9828171ab916d73e8
moronialanis <moronialanis23@gmail.com>
2025-01-30 17:38:00 -0800

    change

--- week04/scripts/arrays.js
+++ week04/scripts/arrays.js
@@ -1,7 +1,7 @@
 //  arrays.js
 const steps = ["one", "two", "three"];
 const listTemplate(step) {
-    return '<li>${step}</li>'; 
+    return <li>${step}</li>; 
 }
 const stepsHtml = steps.map(listTemplate);// use map to convert the list from strings to HTML
 document.querySelector("#myList").innerHTML = stepsHtml.join();// set the innerHTML
