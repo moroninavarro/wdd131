e9cd428a0bfcb7d39b33d6d45b1a5b9c2d4e68a1
moronialanis <moronialanis23@gmail.com>
2025-02-05 14:24:33 -0800

    change

--- week05/scripts/bookCopy.js
+++ week05/scripts/bookCopy.js
@@ -1,12 +1,10 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-
-const li = document.createElement('li');
-
+const li = document.querySelector('li')
 const deleteButton = document.createElement('button');
 
-
+//check
 let chaptersArray = getChapterList() || [];
 
 chaptersArray.forEach(chapter => {
@@ -14,39 +12,47 @@ chaptersArray.forEach(chapter => {
     
 });
 
+
+//check
+button.addEventListener('click', () =>{
+    if (input.value !==''){
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value == '';
+        input.focus();
+    }
+})
+
 function displayList(item){
     let li = document.createElement('li');
-    deleteButton = document.createElement('button');
-    li.TheContent = item;
-    deleteButton.TheContent = '❌';
-    deleteButton.classList.add('eliminate')
+    let deleteButton = document.createElement('button');
+    li.texContent = item;
+    deleteButton.texContent = '❌';
+    deleteButton.classList.add('delete')
     li.append(deleteButton);
     li.append(li);
-    button.addEventListener('click', function(){
+    deleteButton.addEventListener('click', function(){
         list.removeChild(li);
-        deleteChapter(li.TheContent);
+        deleteChapter(li.texContent);
         input.focus();
     });
     console.log('Hello, I am trying to understand (:')
 }
 
 
-function ChapterList(){
-
+function setChapterList(){
     localStorage.setItem('myBOMList', JSON.stringify(chaptersArray));
 }
 
-
 function getChapterList(){
-
     return JSON.parse(localStorage.getItem('myBOMList'));
-
 }
 
 function deleteChapter(chapter){
     chapter = chapter.slice(0,chapter.length - 1);
     chaptersArray = chaptersArray.filter((item) => item !== chapter);
-    ChapterList();
+    setChapterList();
 }
 
 li. textContent = input.value;
@@ -56,15 +62,10 @@ deleteButton.textContent = '❌';
 li.append(deleteButton);
 li.append(li);
 
-button.addEventListener('click', function(){
-    if (input.value !==''){
-        displayList(input.value);
-        chaptersArray.push(input.value);
-        ChapterList();
-        input.value == '';
-        input.focus();
-    }
-})
+
+
+
+
 
 deleteButton.addEventListener('click', function(){
     list.removeChild(li);
